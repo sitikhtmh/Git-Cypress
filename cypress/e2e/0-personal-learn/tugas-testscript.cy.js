0c98807ff269033e0360df5301cf228b5c5966b0
sitikhotimah02 <siti.khotimah.ft17@mail.umy.ac.id>
2023-02-13 15:08:46 +0700

    Menambahkan tugas test script

---
+++ cypress/e2e/0-personal-learn/tugas-testscript.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('clicking "type" navigate to a new url', () => {
+      cy.visit('https://example.cypress.io')
+      //cy.pause()
+      cy.contains('type').click();
+  
+      //Should be on a new URL which includes '/commands/actions'
+      cy.url().should('include', '/commands/actions')
+
+      //get an input, type into it and verify that the value has been update
+      cy.get('.action-email')
+      .type('siti@gmail.com')
+      .should('have.value',"siti@gmail.com")
+    });
+  });
+  

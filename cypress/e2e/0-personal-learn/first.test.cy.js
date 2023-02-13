645b100adabe72dcd1a44feca20fb7d43eb149fc
sitikhotimah02 <siti.khotimah.ft17@mail.umy.ac.id>
2023-02-13 15:07:15 +0700

    Menambahkan test script

---
+++ cypress/e2e/0-personal-learn/first.test.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+  it.only('clicking "get" navigate to a new url', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('h1').contains('Kitchen Sink')
+    cy.contains('get').click();
+
+    //Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/querying')
+  });
+});
